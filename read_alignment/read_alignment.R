6d2026a03bf6b4e132fd878dda7e9debd3729e0f
Rich Demko <rdemko2332@gmail.com>
2021-06-08 12:29:25 -0400

    Removed Positions

--- read_alignment/read_alignment.R
+++ read_alignment/read_alignment.R
@@ -1,45 +1,98 @@
 library(magrittr)
 
 read_alignment <- function(file){
-    raw_data <- readLines( file, warn = FALSE )
+    # readLines appears to separate lines so there's a away to use them independently
+    raw_data <- readLines(file, warn = FALSE )
+    # Makes an empty array
     seq_vector <- c()
+    # Makes an empty character string
     seq_name <- ""
+    # All about getting the organism identifiers in a vector
     for (line in raw_data){
         # New sequence record? Reset numbering
+      # If the lines begins with a ">"
         if ( grepl("^>", line) ){
+          # Then the entire line (surrounded in "") is set  equal to seq_name)
             seq_name <- sub("^>", "", line)
+            # Makes an empty vector of the organism identifiers
             seq_vector[seq_name] <- ""
         }
+      # For actual sequence not identifiers
         else {
             temp_seq <- gsub(" ","",line)
             temp_seq <- gsub("\n","",temp_seq)
-            seq_vector[seq_name] <- paste( seq_vector[seq_name], temp_seq, sep="" )
+            seq_vector[seq_name] <- paste(seq_vector[seq_name], temp_seq, sep="" )
         }
     }
     # Is this an alignment?
+    # Separates the string of proteins by "", allowing the number to be counted
     seq_list <- strsplit(seq_vector, split = "")
     lengths <- sapply(seq_list, length)
+    # Check to make sure all alignments have the same number of proteins
     if ( sum(lengths != lengths[1]) != 0 )
         stop("Your provided file is not an alignment. Please provide an alignment file in FASTA format to use alignfigR.")
-    
-    ############# LOOKIE HERE!!!!!!! ##############
-    # this function used to end above this line and return `seq_list`
-    # this morning 6/3/21 i'm making it into a tibble for a bit more convenience.
-    # You'll note that I NEVER LOAD A LIBRARY (ish) in this script, but I use the "colon colon" :: to refer to functions based on their package.
-    # For now, we load magrittr to be able to use pipe though.
-    # This Is The Way of package writing.
-        
-    # Instead return as a WIDE tibble (this is new code):
-    tibble::as_tibble(seq_list) %>%
-      dplyr::mutate(position = 1:dplyr::n()) %>% # n() is a dplyr function! dplyr::n
-      dplyr::select(position, dplyr::everything()) # everything() is a dplyr function! Parentheses=function.
+    # Instead return as a WIDE tibble
+    tibble::as_tibble(seq_list) 
 }
 
-# Want to mess around and write your own function to do this? 100000% !!!! 
-# The code is your oyster. 
-
-
 # Example usage:
-read_alignment("protein.fasta") -> protein_seqs
-read_alignment("nucleotide.fasta") -> nuc_seqs
+read_alignment("~/Desktop/alignfigR/read_alignment/protein.fasta") -> protein_seqs
+protein_seqs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
