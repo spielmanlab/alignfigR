fdb6ca003884757e3dc7424ee9c493a5b33c08a2
Rich Demko <rdemko2332@gmail.com>
2021-06-16 13:36:29 -0400

    Added multiple palettes

--- alignfigR.R
+++ alignfigR.R
@@ -84,37 +84,35 @@ extract_subalign_improved <- function(alignment, tlist = c(), texcl = FALSE) {
                   # Creates a new column where y2 is 1 greater than y1
                   y2 = y1 + 1)
 }
-extract_subalign_improved(tibble_fasta) -> d
-d
+
 #-------------------------------------------------------------------------------------------------------------
-unique(d$seq) -> define_palette_test
 
 define_palette <- function(typemsa, uniques = NA, custom_colors = NA){
-  # if typemsa is equal to random
+    # if typemsa is equal to random
   if (tolower(typemsa) == "random") {
-    # set subcolors equal to all colors except the null color
+    # subcolors is equal to colors without null_color
     subcolors <- colors()[colors() != null_color]
-    # set palette equal to a sampling of subcolors, the same number as uniques
+    # palette is equal to a random sampling of subcolors, the same length as uniques
     palette <- sample(subcolors, length(uniques))
-    # sets names of palette equal to uniques
+    # names of the palette are set equal to uniques
     names(palette) <- uniques
-  } # if typemsa is 'dna' or 'rna'
-  else if (tolower(typemsa) == "dna" || tolower(typemsa) == "rna"){
-    # set bases equal to ACGTU
+    # if typemsa is equal to 'dna' or 'rna'
+  } else if (tolower(typemsa) == "dna" || tolower(typemsa) == "rna"){
+    # bases is defined as ACGTU
     bases <- c("A", "C", "G", "T", "U")
-    # set palette as a set of 5 colors (two the same for T and U)
+    # palette is defined as a string of 5 colors (two the same for T and U)
     palette <- c("mediumblue", "orangered1", "limegreen", "khaki1", "khaki1")
-    # sets names of palette equal to bases
+    # Names of the palette are defined as the bases
     names(palette) <- bases  
-  } # if typemsa is equal to 'custom'
-  else if (tolower(typemsa) == "custom") {
-    # sets palette equal to custom colors
+    # if typemsa is equal to custom
+  } else if (tolower(typemsa) == "custom") {
+    # palette is defined as custom_colors
     palette <- custom_colors
-    # assigns uniques as the names of the palette
+    # names of the palette are defined as the bases
     names(palette) <- uniques
-  } # if typemsa is equal to 'free'
-  else if (tolower(typemsa) == "free") {
-    # Sets palette as a defined color for each protein/nucleotide
+    # if typemsa is equal to 'free'
+  } else if (tolower(typemsa) == "free") {
+    # the palette is defined below
     palette <- c("A" = "limegreen", "G" = "lightgreen",
                  "C" = "hotpink1", "T" = "red", 
                  "U" = "lightsalmon", "J" = "maroon",
@@ -129,6 +127,74 @@ define_palette <- function(typemsa, uniques = NA, custom_colors = NA){
                  "K" = "orange", "R" = "lightgoldenrod",
                  "P" = "salmon", "-" = null_color,
                  "S" = "darkred", "X" = "black") 
+    # if typemsa is equal to 'ocean'
+  } else if (tolower(typemsa) == "ocean") {
+    # the palette is defined below
+    palette <- c("A" = "turquoise", "G" = "lightgreen",
+                 "C" = "turquoise4", "T" = "tan4", 
+                 "U" = "steelblue", "J" = "springgreen4",
+                 "B" = "seashell", "O" = "seashell4",
+                 "D" = "seagreen", "E" = "lightseagreen", 
+                 "N" = "skyblue", "Q" = "skyblue4",
+                 "I" = "lightblue1", "L" = "lightcyan", 
+                 "M" = "midnightblue", "V" = "powderblue",
+                 "F" = "blue", "W" = "dodgerblue", 
+                 "Y" = "navajowhite", "V" = "moccasin",
+                 "H" = "navy", "Z" = "tan",
+                 "K" = "lavenderblush2", "R" = "lightgoldenrod",
+                 "P" = "deepskyblue4", "-" = null_color,
+                 "S" = "honeydew3", "X" = "aquamarine2") 
+    # if typemsa is equal to 'forest'
+  } else if (tolower(typemsa) == "forest") {
+    # the palette is defined below
+    palette <- c("A" = "wheat4", "G" = "darkgreen",
+                 "C" = "saddlebrown", "T" = "tan4", 
+                 "U" = "brown4", "J" = "springgreen4",
+                 "B" = "black", "O" = "goldenrod4",
+                 "D" = "seagreen", "E" = "chocolate4", 
+                 "N" = "darkseagreen", "Q" = "bisque4",
+                 "I" = "burlywood4", "L" = "peru", 
+                 "M" = "forestgreen", "V" = "rosybrown4",
+                 "F" = "olivedrab4", "W" = "green4", 
+                 "Y" = "navajowhite4", "V" = "moccasin",
+                 "H" = "khaki4", "Z" = "tan",
+                 "K" = "sienna4", "R" = "beige",
+                 "P" = "gray20", "-" = null_color,
+                 "S" = "darkseagreen4", "X" = "peachpuff4") 
+    # if typemsa is equal to 'fire'
+  } else if (tolower(typemsa) == "fire") {
+    # the palette is defined below
+    palette <- c("A" = "tomato", "G" = "orange",
+                 "C" = "yellow", "T" = "red", 
+                 "U" = "brown", "J" = "tan",
+                 "B" = "saddlebrown", "O" = "goldenrod",
+                 "D" = "salmon", "E" = "chocolate", 
+                 "N" = "gray45", "Q" = "paleturquoise",
+                 "I" = "burlywood4", "L" = "peru", 
+                 "M" = "firebrick4", "V" = "sienna4",
+                 "F" = "sandybrown", "W" = "gray20", 
+                 "Y" = "red4", "V" = "indianred",
+                 "H" = "khaki", "Z" = "moccasin",
+                 "K" = "sienna", "R" = "blanchedalmond",
+                 "P" = "lightgoldenrod", "-" = null_color,
+                 "S" = "wheat", "X" = "black")
+    # if typemsa is equal to floral
+  } else if (tolower(typemsa) == "floral") {
+    # the palette is defined below
+    palette <- c("A" = "chartreuse2", "G" = "orange",
+                 "C" = "yellow", "T" = "red", 
+                 "U" = "pink", "J" = "dodgerblue",
+                 "B" = "plum", "O" = "thistle",
+                 "D" = "salmon", "E" = "darkblue", 
+                 "N" = "violet", "Q" = "paleturquoise",
+                 "I" = "steelblue", "L" = "peru", 
+                 "M" = "purple", "V" = "purple4",
+                 "F" = "palevioletred", "W" = "midnightblue", 
+                 "Y" = "palegreen", "V" = "white",
+                 "H" = "lavender", "Z" = "honeydew2",
+                 "K" = "darkorange3", "R" = "aquamarine",
+                 "P" = "mistyrose3", "-" = null_color,
+                 "S" = "wheat", "X" = "black")
   }
   # returns palette
   palette
@@ -173,7 +239,7 @@ plot_alignment <- function(alignment, tlist = c(), texcl = FALSE, typemsa, uniqu
   # returns the plot
   plot
 }
-plot_alignment(tibble_fasta, typemsa = "free", taxon_labels = TRUE, graph_title = "Graph", legend_title = "legend")  
+plot_alignment(tibble_fasta, typemsa = "floral", taxon_labels = TRUE, graph_title = "Graph", legend_title = "legend")  
   
   
 #------------------------------------------------------------------------------------------------------------
