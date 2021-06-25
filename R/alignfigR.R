411ae7fec7cdbf537bfd71b3d02324041d3bcae8
Rich Demko <rdemko2332@gmail.com>
2021-06-25 12:54:06 -0400

    Format Changes and restlying of define_palette

--- R/alignfigR.R
+++ R/alignfigR.R
@@ -1,7 +1,7 @@
 #' Transforms fasta into a tibble
 #'
-#' @param file The fatsa
-#' @return Returns a tibble of the fatsa data
+#' @param file The fasta
+#' @return Returns a tibble of the fasta data
 read_alignment <- function(file) {
   # separates lines so there's a away to use them independently
   raw_data <- readLines(file,
@@ -18,13 +18,17 @@ read_alignment <- function(file) {
     else {
       temp_seq <- gsub(" ","",line)
       temp_seq <- gsub("\n","",temp_seq)
-      seq_vector[seq_name] <- paste(seq_vector[seq_name], temp_seq, sep="" )
+      seq_vector[seq_name] <- paste(seq_vector[seq_name], 
+                                    temp_seq, 
+                                    sep="" )
     }
   }
   # Is this an alignment?
   # Separates the string of proteins by "", allowing the number to be counted
-  seq_list <- strsplit(seq_vector, split = "")
-  lengths <- sapply(seq_list, length)
+  seq_list <- strsplit(seq_vector, 
+                       split = "")
+  lengths <- sapply(seq_list, 
+                    length)
   # Check to make sure all alignments have the same number of proteins
   if ( sum(lengths != lengths[1]) != 0 )
     stop("Your provided file is not an alignment. Please provide an alignment file in FASTA format to use alignfigR.")
@@ -32,7 +36,8 @@ read_alignment <- function(file) {
   tibble::as_tibble(seq_list) -> new_data
   new_data %>%
     dplyr::mutate(column = 1:nrow(new_data)) %>%
-    dplyr::select(column, dplyr::everything())
+    dplyr::select(column, 
+                  dplyr::everything())
 }
 
 
@@ -47,7 +52,11 @@ read_alignment <- function(file) {
 #' @param clist The columns you wish to have portrayed in the plot
 #' @param cexcl Determinant if you want to include only the columns in clist, or exclude only the clist
 #' @return Returns a tibble which contains the preferred taxa and columns, and is prepped for geom_rect()
-extract_subalign <- function(alignment, tlist = c(), texcl = FALSE, clist = c(), cexcl = FALSE) {
+extract_subalign <- function(alignment, 
+                             tlist = c(), 
+                             texcl = FALSE, 
+                             clist = c(), 
+                             cexcl = FALSE) {
   if (length(tlist) == 0){
     alignment -> data
     # If texcl is true, then if will remove all taxa in tlist from the data
@@ -73,7 +82,8 @@ extract_subalign <- function(alignment, tlist = c(), texcl = FALSE, clist = c(),
   number_of_rows/as.integer(dplyr::count(unique(data_alphabetical[1]))) -> length_of_taxa
   data_alphabetical %>%
     # Creates a new column where is it repeating 1:length_of_taxa until it reaches the end
-    dplyr::mutate(x1 = rep(1:length_of_taxa, number_of_rows/length_of_taxa),
+    dplyr::mutate(x1 = rep(1:length_of_taxa, 
+                           number_of_rows/length_of_taxa),
                   # Creates a new column where x2 is 1 greater than x1
                   x2 = x1 + 1,
                   # Creates a new column where 1 is assigned to the first taxon, 2 to the 2nd and so on
@@ -86,7 +96,8 @@ extract_subalign <- function(alignment, tlist = c(), texcl = FALSE, clist = c(),
   } else {
     # replaces 'column' column so it can be used to filter the data
     data_rect %>%
-      dplyr::mutate(column = rep(1:length_of_taxa, number_of_rows/length_of_taxa)) -> data_column_ready
+      dplyr::mutate(column = rep(1:length_of_taxa, 
+                                 number_of_rows/length_of_taxa)) -> data_column_ready
     if (cexcl) {
       # filters the data for all columns that are not mentioned in clist
       data_column_ready %>%
@@ -107,14 +118,15 @@ extract_subalign <- function(alignment, tlist = c(), texcl = FALSE, clist = c(),
 
 
 
-
 #' Assigns the color palette for plot_alignment(), and allows you to select a custom palette
 #'
 #' @param typemsa The palette you wish to use. Options are "random", "dna", "rna", "custom", "free", "ocean", "fire", "forest" and "floral".
 #' @param uniques The protein/nucleotide identifiers used in your data
 #' @param custom_colors A string of the colors you wish to have in the palette that contains the same amount of colors as unique protein/nucleotide identifiers you have in your data. The first identifier in uniques will be assigned the first color in custom_colors and so on.
 #' @return Returns a color palette
-define_palette <- function(typemsa, uniques = NA, custom_colors = NA){
+define_palette <- function(typemsa, 
+                           uniques = NA, 
+                           custom_colors = NA){
   if (tolower(typemsa) == "random") {
     # subcolors is equal to colors without null_color
     subcolors <- grDevices::colors()[grDevices::colors() != null_color]
@@ -122,34 +134,33 @@ define_palette <- function(typemsa, uniques = NA, custom_colors = NA){
     palette <- sample(subcolors,
                       length(uniques))
     names(palette) <- uniques
+    return(palette)
   } else if (tolower(typemsa) == "dna" || tolower(typemsa) == "rna"){
-    palette <- nucleotide_pal
-    # Names of the palette are defined as the bases
-    names(palette) <- nucleotides
+    return(nucleotide_pal)
   } else if (tolower(typemsa) == "custom") {
     # palette is defined as custom_colors
     palette <- custom_colors
     # names of the palette are defined as the bases
     names(palette) <- uniques
+    return(palette)
   } else if (tolower(typemsa) == "free") {
     # the palette is defined below
-    palette <- free_pal
+    return(free_pal)
   } else if (tolower(typemsa) == "ocean") {
     # the palette is defined below
-    palette <- ocean_pal
+    return(ocean_pal)
   } else if (tolower(typemsa) == "forest") {
     # the palette is defined below
-    palette <- forest_pal
+    return(forest_pal)
   } else if (tolower(typemsa) == "fire") {
     # the palette is defined below
-    palette <- fire_pal
+    return(fire_pal)
   } else if (tolower(typemsa) == "floral") {
     # the palette is defined below
-    palette <- floral_pal
+    return(floral_pal)
   } else {
     stop("Not a valid palette.")
   }
-  palette
 }
 
 
@@ -171,18 +182,39 @@ define_palette <- function(typemsa, uniques = NA, custom_colors = NA){
 #' @param graph_title Sets the graph title
 #' @param legend_title Sets the legend title
 #' @return Returns an MSA of the data
-plot_alignment <- function(alignment, tlist = c(), texcl = FALSE, clist = c(), cexcl = FALSE, typemsa, uniques = NA, custom_colors = NA, taxon_labels = FALSE, graph_title = NA, legend_title = NA) {
+plot_alignment <- function(alignment, 
+                           tlist = c(), 
+                           texcl = FALSE, 
+                           clist = c(), 
+                           cexcl = FALSE, 
+                           typemsa, 
+                           uniques = NA, 
+                           custom_colors = NA, 
+                           taxon_labels = FALSE, 
+                           graph_title = NA, 
+                           legend_title = NA) {
   # runs extract_subalign and defines it as plot_frame
-  extract_subalign(alignment, tlist, texcl, clist, cexcl) -> plot_frame
+  extract_subalign(alignment, 
+                   tlist, 
+                   texcl, 
+                   clist, 
+                   cexcl) -> plot_frame
   # defines uniques as the uniques of the sequence in plot_frame
   unique(plot_frame$seq) -> uniques
   # runs define palette and sets it's output as pal
-  define_palette(typemsa, uniques, custom_colors) -> pal
+  define_palette(typemsa, 
+                 uniques, 
+                 custom_colors) -> pal
   if (taxon_labels == FALSE){
     plot <- ggplot2::ggplot() +
       # geom_rect() using plot_frame from extract_subalign()
-      ggplot2::geom_rect(plot_frame, mapping= ggplot2::aes(xmin=x1-1, xmax=x2-1, ymin=
-                                  y1-1, ymax=y2-1, fill = seq), linetype=0) +
+      ggplot2::geom_rect(plot_frame, 
+                         mapping= ggplot2::aes(xmin=x1-1, 
+                                               xmax=x2-1, 
+                                               ymin= y1-1, 
+                                               ymax=y2-1, 
+                                               fill = seq), 
+                        linetype=0) +
       # sets the custom color palette
       ggplot2::scale_fill_manual(values=pal,
                         name = legend_title) +
@@ -190,8 +222,13 @@ plot_alignment <- function(alignment, tlist = c(), texcl = FALSE, clist = c(), c
   } else {
     plot <- ggplot2::ggplot() +
       # geom_rect() being run on plot_frame from extract_subalign()
-      ggplot2::geom_rect(plot_frame, mapping= ggplot2::aes(xmin=x1, xmax=x2, ymin =
-                                          y1, ymax=y2, fill = seq), linetype=0) +
+      ggplot2::geom_rect(plot_frame, 
+                         mapping= ggplot2::aes(xmin=x1, 
+                                               xmax=x2, 
+                                               ymin = y1, 
+                                               ymax=y2, 
+                                               fill = seq), 
+                         linetype=0) +
       ggplot2::labs(title = graph_title) +
       # defines custom color palette and legend title
       ggplot2::scale_fill_manual(values=pal,
@@ -211,7 +248,8 @@ plot_alignment <- function(alignment, tlist = c(), texcl = FALSE, clist = c(), c
 #' @param alignment_tibble The tibble from read_alignment()
 #' @param column_of_interest The specific column you want to see
 #' @return Returns a tibble with rows 'value', 'count' and 'percent'
-calculate_column_percentage <- function(alignment_tibble, column_of_interest) {
+calculate_column_percentage <- function(alignment_tibble, 
+                                        column_of_interest) {
   alignment_tibble %>%
     # Filters the data where column is equal to the column of interest
     dplyr::filter(column == column_of_interest) %>%
