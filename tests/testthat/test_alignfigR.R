40ea43690dae5d3d20248dba40a2c2775e383b0a
Rich Demko <rdemko2332@gmail.com>
2021-06-25 12:57:47 -0400

    Added tests for define_palette

--- tests/testthat/test_alignfigR.R
+++ tests/testthat/test_alignfigR.R
@@ -1,7 +1,32 @@
 # Tests
-
+# Define Palette Tests
+test_that("test that define_palette() returns the correct palette", {
+  expect_equal(define_palette("dna"), nucleotide_pal)
+  expect_error(define_palette("notapalette"))
+})
+test_that("test that define_palette() returns the correct palette", {
+  expect_equal(define_palette("rna"), nucleotide_pal)
+  expect_error(define_palette("notapalette"))
+})
+test_that("test that define_palette() returns the correct palette", {
+  expect_equal(define_palette("free"), free_pal)
+  expect_error(define_palette("notapalette"))
+})
+test_that("test that define_palette() returns the correct palette", {
+  expect_equal(define_palette("ocean"), ocean_pal)
+  expect_error(define_palette("notapalette"))
+})
+test_that("test that define_palette() returns the correct palette", {
+  expect_equal(define_palette("forest"), forest_pal)
+  expect_error(define_palette("notapalette"))
+})
+test_that("test that define_palette() returns the correct palette", {
+  expect_equal(define_palette("fire"), fire_pal)
+  expect_error(define_palette("notapalette"))
+})
 test_that("test that define_palette() returns the correct palette", {
   expect_equal(define_palette("floral"), floral_pal)
   expect_error(define_palette("notapalette"))
 })
+
 
