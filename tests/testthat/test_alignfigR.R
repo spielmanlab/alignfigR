2e07262012fd458984c93bbb58c0b15da682c3ff
Rich Demko <rdemko2332@gmail.com>
2021-06-29 10:26:11 -0400

    Reworded 'typemsa' to 'palette_msa'

--- tests/testthat/test_alignfigR.R
+++ tests/testthat/test_alignfigR.R
@@ -59,7 +59,7 @@ test_that("test that the tlist in extract_subalign() functions correctly", {
 test_that("test that define_palette() returns the correct palette", {
   expect_equal(define_palette("dna"), nucleotide_pal)
   expect_equal(define_palette("rna"), nucleotide_pal)
-  expect_equal(define_palette("free"), free_pal)
+  expect_equal(define_palette("basic"), basic_pal)
   expect_equal(define_palette("ocean"), ocean_pal)
   expect_equal(define_palette("forest"), forest_pal)
   expect_equal(define_palette("fire"), fire_pal)
