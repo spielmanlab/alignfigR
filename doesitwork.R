d2925b31b5d6d41864d9899963e70e9bd68ab660
Rich Demko <rdemko2332@gmail.com>
2021-06-30 17:08:22 -0400

    'Stack = TRUE' added to tests

--- doesitwork.R
+++ doesitwork.R
@@ -1,6 +1,32 @@
 # Use this script for playing around
 load_all()
-read_alignment("Data/protein.fasta") -> tibble_fasta
-plot_alignment(tibble_fasta, typemsa = "free", taxon_labels = TRUE, graph_title = "New Graph", legend_title = "New Legend")
+read_alignment("tests/testthat/Data/protein.fasta") -> tibble_fasta
+plot_alignment(tibble_fasta, palette_msa = "forest")
 define_palette("dna") -> test_palette
 test_palette
+data.class(1)
+tibble_fasta
+
+plot_alignment(tibble_fasta, taxon_labels = TRUE, palette_msa = "floral", graph_title = "Graph", legend_title = "Legend", clist = c(1:260), stack = TRUE) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
