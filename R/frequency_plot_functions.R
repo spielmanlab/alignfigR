6e5b28b57837a30757c8f1e9707cc1dd71250097
Rich Demko <rdemko2332@gmail.com>
2021-07-13 13:32:01 -0400

    Finished consensus Functionality

--- R/frequency_plot_functions.R
+++ R/frequency_plot_functions.R
@@ -18,10 +18,12 @@ create_site_frequencies <- function(data_longer,
   data_alphabetical %>%
     dplyr::mutate(column = rep(1:length_of_taxa,
                                number_of_rows/length_of_taxa)) %>%
-  ggplot2::ggplot() +
-    ggplot2::aes(x = column,
-                 fill = seq) +
-    ggplot2::geom_bar(position = "stack") +
+    group_by(column, seq) %>%
+    summarise(count = n()) %>%
+    mutate(percent = count/sum(count)) %>%
+    ggplot() +
+    aes(x = column, y = percent, fill = seq) +
+    geom_bar(stat = "identity") +
     ggplot2::scale_fill_manual(values = pal,
                                name = legend_title)+
     ggplot2::labs(title = graph_title) +
